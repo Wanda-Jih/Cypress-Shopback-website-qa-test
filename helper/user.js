898e2f1c2921f8bcd70c03afc41c39545bed26a8
Wanda-Jih <43124480+Wanda-Jih@users.noreply.github.com>
2020-11-11 16:34:49 +0800

    cancel the real accounts

--- helper/user.js
+++ helper/user.js
@@ -1,12 +1,8 @@
 //helper/user.js
 export default{
-    // userTW1:{account:"user1", password :"123"},
-    // userTW2:{account:"user2", password :"123"},
-    // userTW3:{account:"user3", password :"123"}
+    userTW1:{account:"user1", password :"123"},
+    userTW2:{account:"user2", password :"123"},
+    userTW3:{account:"user3", password :"123"}
 
-
-    userTW1:{account:"allen@shopback.com", password :"123"},
-    userTW2:{account:"andy.li@shopback.com",password:"abcd1234"},
-    userTW3:{account:"andy.test@shopback.com",password:"abcd1234"}
 };
 
